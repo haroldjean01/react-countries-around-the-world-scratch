311e72adcb99e6ccc8919528b3d183e85dd3f61a
Harold Jean <haroldjean38@yahoo.com>
2023-01-18 17:58:54 -0500

    created files in components and hooks in src

---
+++ src/components/Main/Main.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import useCountries from '../../hooks/useCountries.js';
+
+export default function Main() {
+  const countries = useCountries();
+  return <div></div>;
+}
---
+++ src/hooks/useCountries.js
@@ -0,0 +1,15 @@
+import React, { useEffect, useState } from 'react';
+import { fetchCountries } from '../services/fetchCountries.js';
+
+export default function useCountries() {
+  const [countries, setCountries] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const resp = await fetchCountries();
+      setCountries(resp);
+    };
+    fetchData();
+  }, []);
+  return countries;
+}
