1b6f198de63f532bd1656ade04e1e5af9cd35c6c
Harold Jean <haroldjean38@yahoo.com>
2023-01-18 18:36:44 -0500

    updated CountryCard with name and image params

--- src/components/CountryCard/CountryCard.js
+++ src/components/CountryCard/CountryCard.js
@@ -1,5 +1,10 @@
 import React from 'react';
 
-export default function CountryCard() {
-  return <div>CountryCard</div>;
+export default function CountryCard({ name, iso2 }) {
+  return (
+    <div className="country-card">
+      <h2>{name}</h2>
+      <img src={`https://flagcdn.com/64x48/${iso2.toLowerCase()}.png`} />
+    </div>
+  );
 }
