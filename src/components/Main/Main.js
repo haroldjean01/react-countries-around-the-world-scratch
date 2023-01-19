070bed1529d95e77ed359756b1f6ee1b23f65c7d
Harold Jean <haroldjean38@yahoo.com>
2023-01-18 19:09:37 -0500

    added try catch error to Main.js

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -3,9 +3,10 @@ import useCountries from '../../hooks/useCountries.js';
 import CountryCard from '../CountryCard/CountryCard.js';
 
 export default function Main() {
-  const countries = useCountries();
+  const { countries, error } = useCountries();
   return (
     <div>
+      <p style={{ color: 'red' }}>{error}</p>
       {countries.map((country) => (
         <CountryCard key={country.id} {...country} />
       ))}
