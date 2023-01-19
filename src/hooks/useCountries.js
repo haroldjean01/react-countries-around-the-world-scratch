01d2f0145e5d4d1ea08d9da8b5100983781b653b
Harold Jean <haroldjean38@yahoo.com>
2023-01-18 19:04:16 -0500

    added try catch to useEffect in useCountries

--- src/hooks/useCountries.js
+++ src/hooks/useCountries.js
@@ -3,13 +3,18 @@ import { fetchCountries } from '../services/fetchCountries.js';
 
 export default function useCountries() {
   const [countries, setCountries] = useState([]);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
     const fetchData = async () => {
-      const resp = await fetchCountries();
-      setCountries(resp);
+      try {
+        const resp = await fetchCountries();
+        setCountries(resp);
+      } catch (e) {
+        setError('Somethings Wrong Here');
+      }
     };
     fetchData();
   }, []);
-  return countries;
+  return { countries, error };
 }
